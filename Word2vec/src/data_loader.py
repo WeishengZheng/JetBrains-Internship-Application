165c3641a46bd5edc203df895a5d25ad0b6697cc
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-03-10 17:48:27 +0000

    Using with to open the file

--- Word2vec/src/data_loader.py
+++ Word2vec/src/data_loader.py
@@ -2,13 +2,10 @@ from datasets import load_dataset
 import re
 
 def load_and_storage():
-    try:
-        f = open("../Data/dataset", "w")
+    with open("../Data/dataset", "w") as f:
         dataset = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
         for i in dataset:
             f.write(i["text"])
-    finally:
-        f.close()
 
 if __name__ == "__mian__":
     load_and_storage()

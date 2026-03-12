dedb486f4ea382ff7e8613e3ded6a32a1550a2e2
Weisheng Zheng <128396359+WeishengZheng@users.noreply.github.com>
2026-03-12 12:21:26 +0000

    Add foward

---
+++ Word2vec/src/backpropagation.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, embeddings_size : int, vocabulary_size : int):
+        self.embeddings_size = embeddings_size
+        self.vocabulary_size = vocabulary_size
+
+        self.w_input= np.random.randn(vocabulary_size, embeddings_size) * 0.01
+        self.w_output = np.random.randn(embeddings_size, vocabulary_size) * 0.01
+
+    def softmax(self, logits):
+        exp_logits = np.exp(logits - np.max(logits))
+        return exp_logits / np.sum(exp_logits)
+        
+    def foward(self, input_vector : list):
+        input_hidden = np.mean(self.w_input[input_vector], axis = 0)
+        logits = np.dot(input_hidden, self.w_output)
+        return self.softmax(logits)
+    
+    def prediction(self, input_vector : list) -> int:
+        return np.argmax(self.foward(input_vector))
+
+
+    
